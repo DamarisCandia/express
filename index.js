ae2add9162b8ac2e54647b9c32af6640b6de7a40
DamarisCandia <102555525+DamarisCandia@users.noreply.github.com>
2023-10-02 10:05:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -25,14 +25,64 @@ app.put('/updateaviones',(req,res) =>{
     res.send('Listar aviones');
 })
 
+app.add('/postavion',(req,res) =>{
+    res.send('creando avion');
+})
+
+app.delete('/deleteavion', (req,res) =>{
+    res.send('eliminando avion')
+})
+
 //AUTOS
 app.get('/autos',(req,res) =>{
     res.send('Listar autos');
 })
 
+app.put('/updateautos',(req,res) =>{
+    res.send('Listar autos');
+})
+
+app.add('/postauto',(req,res) =>{
+    res.send('creando auto');
+})
+
+app.delete('/deleteauto', (req,res) =>{
+    res.send('eliminando auto')
+})
+
 //MOTOS
 app.get('/motos',(req,res) =>{
     res.send('Listar motos');
+})
+
+app.put('/updatemotos',(req,res) =>{
+    res.send('Listar motos');
+})
+
+app.add('/postauto',(req,res) =>{
+    res.send('creando moto');
+})
+
+app.delete('/deletmoto', (req,res) =>{
+    res.send('eliminando moto')
+})
+
+//TRENES
+
+app.get('/trenes',(req,res) =>{
+    res.send('Listar trenes');
+})
+
+app.put('/updatetrenes',(req,res) =>{
+    res.send('Actualizar trenes');
+})
+
+app.add('/posttren',(req,res) =>{
+    res.send('creando tren');
+})
+
+app.delete('/deletetren', (req,res) =>{
+    res.send('eliminando trem')
 })
 
 //configuro el puerto por el cual se va a estar 'escuchando' solicitudes
